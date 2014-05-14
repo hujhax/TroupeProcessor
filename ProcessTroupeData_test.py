73e0847fcce3e92dbe8afb1705dad430bd8e0ae9
Peter Rogers <peter.rogers@mtuity.com>
2014-05-14 01:59:47 -0500

    Make it so we can test the number of items in the resulting dictionary, if we like.

--- ProcessTroupeData_test.py
+++ ProcessTroupeData_test.py
@@ -4,11 +4,15 @@ import unittest
 
 class SimpleValidDatabases(unittest.TestCase):
 
-    def validate_troupe_data(self, file_name, name, test_data):
+    def validate_troupe_data(self, file_name, name, test_data,
+                             num_troupes=None):
         dict = ProcessTroupeData.process_troupe_data(file_name)
 
         self.assertTrue(name in dict)
         dict_data = dict[name]
+
+        if num_troupes:
+            self.assertEqual(len(dict), num_troupes)
 
         for k, v in test_data:
             self.assertEqual(dict_data[k], v)
@@ -22,7 +26,7 @@ class SimpleValidDatabases(unittest.TestCase):
                      ("deal", "deal1"),
                      ("photo", "www.photo1"),
                      ("video", "www.video1")]
-        self.validate_troupe_data("test/OneRow.ods", "name1", test_data)
+        self.validate_troupe_data("test/OneRow.ods", "name1", test_data, 1)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
